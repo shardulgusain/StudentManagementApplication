2df97e3d281bc232016b9a44ebcc3e0cf3d68222
Shardul Gusain <shardulsgusain1@gmail.com>
2022-12-27 05:21:31 +0530

    Initial Commit

---
+++ StudentManagementApplication/src/studentmanagementapplication/StudentManagementApplication.java
@@ -0,0 +1,18 @@
+
+package studentmanagementapplication;
+
+/**
+ *
+ * @author GUSAIN
+ */
+public class StudentManagementApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
