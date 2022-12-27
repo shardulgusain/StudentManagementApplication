a0f2669b1252848ffe0e9577f8cd6cbc37f83e67
Shardul Gusain <shardulsgusain1@gmail.com>
2022-12-27 05:51:43 +0530

    Student class and database

--- StudentManagementApplication/src/studentmanagementapplication/StudentManagementApplication.java
+++ StudentManagementApplication/src/studentmanagementapplication/StudentManagementApplication.java
@@ -12,7 +12,13 @@ public class StudentManagementApplication {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        while(true)
+        {
+            System.out.println("Press 1 to ADD student");
+            System.out.println("Press 1 to DELETE student");
+            System.out.println("Press 1 to DISPLAY student");
+            System.out.println("Press 1 to DELETE student");
+        }
     }
     
 }
